c899bb439bfd6b90f72b89cf8a240414d0956f32
Anne Lott <anne.lott@duke.edu>
2017-05-25 16:06:06 -0400

    App visualizing Old Faithful wait times

---
+++ Mapmaker/app.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(leaflet)
+library(magrittr)
+
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Map of the World"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         numericInput("latitude", label = "Latitude", min = -90, max = 90, value = 0),
+         numericInput("longitude", label = "Latitude", min = -180, max = 180, value = 0)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         leafletOutput("mymap")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$mymap <- renderLeaflet({
+      leaflet() %>%
+       addTiles() %>%
+       addMarkers(lng = input$longitude, lat = input$latitude, popup = "Location")
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
